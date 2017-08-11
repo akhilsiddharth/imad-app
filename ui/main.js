7a29cdc99b29fd73552ace34ed6d98d81f63069f
akhilsiddharth <pyda.akhil@gmail.com>
2017-08-11 20:46:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var element=document.getElementById('My-text');
-element.innerHTML('New value');
+
 var img=document.getElementbyId('madi');
 img.onclick=function() {
     img.style.marginleft='150px';
